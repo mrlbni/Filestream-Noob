5a8ec54168cecd3172193b4eafebdadcd48b10b7
mrlbni <suchitamiki06@gmail.com>
2025-04-16 20:26:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 4))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://z.noob.web.id")
+    BASE_URL = env.get("BASE_URL", "https://web-production-303d.up.railway.app")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
